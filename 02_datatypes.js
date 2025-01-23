82eaa139c7a998da1976b6f7ddc08d098b3c29f1
Shahil <shahiljsr2503@gmail.com>
2025-01-23 22:02:10 +0530

    about datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+/* PRIMITIVE DATATYPES
+number => 2 to the power 53
+bigint
+string => "written in doublequotes"
+boolean => t or f
+null => standalone value
+undefined => means value is not asinged yet
+symbol => unique
+*/
+
+let state = null
+let  city
+console.log(typeof undefined); // type is same as well undefined
+console.log(typeof null); // type is object
